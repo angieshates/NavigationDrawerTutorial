7badea4f2395dfc907aa84b69608d99e40c3a5fb
Angelica Barreda B <anjelyk.b@gmail.com>
2016-05-07 16:41:23 -0500

    update

--- app/src/main/java/com/compscitutorials/basigarcia/navigationdrawervideotutorial/MainFragment.java
+++ app/src/main/java/com/compscitutorials/basigarcia/navigationdrawervideotutorial/MainFragment.java
@@ -2,14 +2,11 @@ package com.compscitutorials.basigarcia.navigationdrawervideotutorial;
 
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 
 /**
